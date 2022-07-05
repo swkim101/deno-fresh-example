b06a541bbe9d5885913e1be31e879b28fa8d3df3
Sungwoo Kim <git@sung-woo.kim>
2022-07-04 23:33:46 -0400

    github user search
    and finally ignores 508 bugs

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,30 +2,46 @@
 import { h } from "preact";
 import Counter from "../islands/Counter.tsx";
 import { Handlers, PageProps } from "$fresh/server.ts";
-interface Data {
-  joke: string;
+interface User {
+  login: string;
+  name: string;
+  avatar_url: string;
+  results: string;
+  query: string;
 }
 
-export const handler: Handlers<Data> = {
-  async GET(_req, ctx) {
-    const host = Deno.env.get("DASH_DENO_COM") === "TRUE" ? "https://sungwoo-kim.deno.dev" : "http://localhost:8000"
-    const res = await fetch(`${host}/api/joke`)
-    const joke = await res.text()
-    console.log(joke)
-    return ctx.render({ joke });
+
+export const handler: Handlers<User | null> = {
+  async GET(req, ctx) {
+    const url = new URL(req.url);
+    const query = url.searchParams.get("q") || "";
+    const resp = await fetch(`https://api.github.com/users/${query}`);
+    if (resp.status === 404) {
+      return ctx.render(null);
+    }
+    const user: User = await resp.json();
+    return ctx.render(user);
   },
 };
 
-export default function Home({ data }: PageProps<Data>) {
+export default function Home({ data }: PageProps<User | null>) {
+  const query = data?.query;
   return (
     <div>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <div>{ data.joke }</div>
-      <Counter start={3} />
+      <form>
+        <label htmlFor="username">Github username: </label>
+        <input id="username" type="text" name="q" value={query} placeholder="e.g., swkim101" />
+        <button type="submit">Search</button>
+      </form>
+      { data && 
+      <div>
+        <h3>Hello, {data.name}</h3>
+        <img src={data.avatar_url} width={64} height={64} />
+        <div>
+          <a target="_blank" href={`https://github.com/${data.login}?tab=repositories`}>Click to @{data.login}'s repos (new tab)</a>
+        </div>
+      </div>
+      }
     </div>
   );
 }
