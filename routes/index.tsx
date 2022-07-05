241bb702dc491a5f781c8dcf2313466b5219db5f
Sungwoo Kim <git@sung-woo.kim>
2022-07-04 23:04:18 -0400

    bug: Loop Detected
    try to resolve 508: Loop Detected (LOOP_DETECTED) Recursive
    requests to the same deployment cannot be processed.

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,7 +9,7 @@ interface Data {
 
 export const handler: Handlers<Data> = {
   async GET(req, ctx) {
-    const host = Deno.env.get("DASH_DENO_COM") == "TRUE" ? "https://sungwoo-kim.deno.dev" : "http://localhost:8000"
+    const host = Deno.env.get("DASH_DENO_COM") == "TRUE" ? "http://localhost:8000" : "http://localhost:8000"
     const res = await fetch(`${host}/api/joke`)
     const joke = await res.text()
     return ctx.render({ joke });
