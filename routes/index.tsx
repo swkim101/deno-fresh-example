db65ca3524a82fe96a63176824d3312890873f54
Sungwoo Kim <git@sung-woo.kim>
2022-07-04 23:01:32 -0400

    show jokes at the main page

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,23 @@
 /** @jsx h */
 import { h } from "preact";
 import Counter from "../islands/Counter.tsx";
-import Joke from "../islands/Joke.tsx";
-import { handler } from "./api/joke.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Home() {
+interface Data {
+  joke: string;
+}
+
+export const handler: Handlers<Data> = {
+  async GET(req, ctx) {
+    const host = Deno.env.get("DASH_DENO_COM") == "TRUE" ? "https://sungwoo-kim.deno.dev" : "http://localhost:8000"
+    const res = await fetch(`${host}/api/joke`)
+    const joke = await res.text()
+    return ctx.render({ joke });
+  },
+};
+
+
+export default function Home({ data }: PageProps<Data>) {
   return (
     <div>
       <img
@@ -12,10 +25,7 @@ export default function Home() {
         height="100px"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
-      <Joke />
-      <p>
-        file, and refresh.
-      </p>
+      <div>{ data.joke }</div>
       <Counter start={3} />
     </div>
   );
